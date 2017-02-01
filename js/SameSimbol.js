aa66b586353c64142123f8137baa8b56a5d2284f
rogorri <rogorri@gmail.com>
2017-02-01 13:04:05 +0100

    some files deleted

--- js/SameSimbol.js
+++ js/SameSimbol.js
@@ -1,45 +1,82 @@
-
-
-var newgame = new SameSimbol();  //Initialitzed object newgame
-
-
-
 function SameSimbol () {
-  this.points = 0;
-  this.screenNumber;
-  this.checkNumber;
+  this.screenNumber = null;
+  this.checkNumber = null;
+  this.counter= 0;
+  this.clickedID = null;
+  this.finalScore= null;
 }
 
-//variables from SameSimbol game
 
-SameSimbol.prototype.getrandom = function () {
+SameSimbol.prototype._getRandom = function () {
   return Math.floor(Math.random()*2);
 };
 
-// get a random number: 0 or 1
 
-SameSimbol.prototype.start = function() {
-    this.screenNumber = this.getrandom();
-    this.checkNumber
-    document.getElementById("ScreenNumberDiv").innerHTML = this.screenNumber;
-  return this.screenNumber;
-};
+SameSimbol.prototype.start = function (){
+  this.screenNumber = this._getRandom();
+  this.checkNumber = this._getRandom();
+  var interior = this.screenNumber;
+  var interior1 = this.checkNumber;
+      $("#ScreenNumberDiv").append("<img src='images/ficha"+this.checkNumber+".png'  id='card'>");
+      console.log(this.checkNumber);
+      var timeoutId = setTimeout(function () {
+      $("#card").remove();
+      $("#ScreenNumberDiv").append("<img src='images/ficha"+interior+".png'  id='card'>");
+        console.log(interior);
+      }, 2000);
+  };
 
-// start the game with a random number to show in the screen
 
-SameSimbol.prototype.clickOk = function (){
-          if (this.checkNumber === undefined){
-            return "Tell us if the next number is the same than thisone";
-          }
-            else if (this.checkNumber === this.screenNumber) {
-              this.points += 1;
-          return "Very Good";
-          }else{
-          return "Not your best";}
+
+
+SameSimbol.prototype.nextNumber = function () {
   this.checkNumber = this.screenNumber;
-    this.screenNumber = this.getrandom();
+  this.screenNumber = this._getRandom();
+  $("#card").remove();
+  $("#ScreenNumberDiv").append("<img src='images/ficha"+this.screenNumber+".png'  id='card'>");
 };
 
+SameSimbol.prototype.evaluate = function () {
+  if (this.screenNumber === this.checkNumber){
+    if (this.clickedID === "OK") {
+      this.counter ++;
+      document.getElementById("pointer").innerHTML = this.counter;
+      console.log ("Lo has echo bien, ya tienes " + this.counter + " puntos");
+    }
+    else {console.log ("Fallaste, acumulas " + this.counter + " puntos");}
+  }
+  else {
+    if (this.clickedID === "KO"){
+    this.counter ++;
+    document.getElementById("pointer").innerHTML = this.counter;
+    console.log ("Lo has echo bien, ya tienes " + this.counter + " puntos");}
+    else {
+      {console.log ("Fallaste, acumulas " + this.counter + " puntos");
+    }
+  }
+}
+};
+
+SameSimbol.prototype.final = function () {
+this.finalScore = this.counter;
+
+ };
+
+
+var newgame =new SameSimbol();
+newgame.start();
+
+var seconds = 60;
+var intervalId = setInterval(function() {
+  if (seconds > 0) {
+    document.getElementById("counter").innerHTML = seconds;
+  } else {
+    document.getElementById("counter").innerHTML = "Se te acabo el tiempo";
+    clearInterval(intervalId);
+    newgame.final();
+  }
+	seconds--;
+}, 1000);
 
 
 
@@ -47,58 +84,15 @@ SameSimbol.prototype.clickOk = function (){
 
 
 
+$( document ).ready(function() {
+  $(".button").on("click", function (e) {
 
+    console.log("Has hecho click");
+    console.log($(this).attr("id"));
+    newgame.clickedID = $(this).attr("id");
+    console.log("su Id es " + newgame.clickedID);
+    newgame.evaluate();
+    newgame.nextNumber();
+  });
 
-
-// SameSimbol.prototype.numbers = function (){
-//     this.checkNumber = this.screenNumber;
-//     this.screenNumber = this.getrandom();
-//     return this.screenNumber;
-// };
-//
-//
-//
-// SameSimbol.prototype.isright = function(){
-//   if (checkNumber === screenNumber){
-//     this.numbers();
-//       return true;
-//   } else  {
-//     this.numbers();
-//       return false;
-// }
-// };
-//
-//
-// SameSimbol.prototype.clickOK = function (){
-// if (checkNumber === 10){
-//  return "Tell us if the next number is the same than thisone";
-// }
-//   else if (this.isright () === true) {
-//   return "Very Good";
-//   }
-// return "Not your best";
-//
-// };
-
-
-
-
-
-
-// SameSimbol.prototype.start = function() {
-//   firstNumber = this.getrandom();
-//   this.simbols.push(firstNumber);
-//     return firstNumber;
-// };
-//
-// SameSimbol.prototype.nextNumbers = function () {
-//  secondNumber = this.getrandom();
-//  this.simbols.push(secondNumber);
-//     if (this.simbols.length > 2) {
-//         this.simbols.pop();
-//       }
-//         };
-//
-// SameSimbol.prototype.isEqual = function (){
-//   return (this.simbols[0] === this.simbols[1]);
-// };
+});
